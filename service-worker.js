c384c4ad48bfe062ddf1ebe115a1b1d755c8dacf
ushamodi486-alt <ushamodi486@gmail.com>
2026-09-07 00:15:43 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,8 @@ const APP_SHELL = [
   "./style.css",
   "./script.js",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
